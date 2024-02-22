2ddce87ed6f127ddd1e874520566c83d4f356137
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-02-22 23:00:32 +0600

    jwt verify done and modified simply

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 var cors = require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000
 // *********** MIDDLEWARE *********** //
 app.use(cors())
 app.use(express.json())
+
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 app.get('/', (req, res) => {
     res.send('Hemo Hub server is running.')
@@ -39,6 +55,14 @@ async function run() {
         const userCollection = client.db('hemoHubDB').collection('users');
         const donationRequestCollection = client.db('hemoHubDB').collection('donationRequests');
 
+        // ********** JWT Start********** //
+        app.post('/jwt', async (req, res) => {
+            const email = req.body;
+            const token = jwt.sign(email, process.env.ACCESS_TOKEN);
+            res.send({ token });
+        })
+        // ********** JWT End********** //
+
         app.get('/api/v1/divisions', async (req, res) => {
             const result = await divisionCollection.find().toArray();
             res.send(result);
@@ -66,27 +90,36 @@ async function run() {
             }
         })
 
-        app.get('/api/v1/user/:uid', async (req, res) => {
+        app.get('/api/v1/user/:uid', verifyJWT, async (req, res) => {
+            if (!req.decoded.email) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
             const uid = req.params.uid;
             const query = { uid }
             const result = await userCollection.findOne(query);
             res.send(result);
         })
 
-        app.post('/api/v1/create-donation-request', async (req, res) => {
+        app.post('/api/v1/create-donation-request', verifyJWT, async (req, res) => {
+            if (!req.decoded.email) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
             const donationInfo = req.body;
             const result = await donationRequestCollection.insertOne(donationInfo);
             res.send(result);
         })
 
-        app.get('/api/v1/my-donation-request/:email', async (req, res) => {
+        app.get('/api/v1/my-donation-request/:email', verifyJWT, async (req, res) => {
             const email = req.params.email;
+            if (req.decoded.email !== email) {
+                return res.status(401).send({ message: 'unauthorized access' })
+            }
             const query = { requesterEmail: email };
             const result = await donationRequestCollection.find(query).sort({ _id: -1 }).toArray();
             res.send(result);
         })
 
-        app.patch('/api/v1/update-user-info/:id', async (req, res) => {
+        app.patch('/api/v1/update-user-info/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const updatedInfo = req.body;
             const query = { _id: new ObjectId(id) };
